eed378862ab8085b885f03992bde76230437b65a
Demis Struiksma <contact@demisstruiksma.com>
2024-11-16 10:36:12 +0100

    Add register functionality

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,15 +1,39 @@
-import { useState } from 'react';
+import { FirebaseError } from 'firebase/app';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { FormEvent, useState } from 'react';
+import { auth } from '../services/firebase';
 
 function Register() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [firstName, setFirstName] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    
+    const handleRegister = async (event:FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        try {
+            // Create new user with filled in email and password after form submission.
+            await createUserWithEmailAndPassword(auth, email, password);
+        } catch (error: unknown) {
+            // Check if given error is a Firebase (in this case register) error, if so display error message. Otherwise throw an unknown error.
+            if (error instanceof FirebaseError) {                
+                // Capture error message in state to display in UI, if any.
+                setErrorMessage(error.message);
+            } else {
+                console.error('An unknown error occurred', error);
+            }
+        }
+    }
 
     return (
         <main>
-            <form>
-                <input />
-                <input />
+            <form onSubmit={handleRegister}>
+                <label>Email:</label>
+                <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} required/>
+
+                <label>Password</label>
+                <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} required/>
+                <button type="submit">Register</button>
+                {errorMessage && <div>{errorMessage}</div>}
             </form>
         </main>
     );
