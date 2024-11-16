b10976c9d8f24e350da5107a673a7f3cc390f2e6
Demis Struiksma <contact@demisstruiksma.com>
2024-11-16 23:17:02 +0100

    Add firebase config to local environment file

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCdeE3nI-FYoz_1tSRxmf6L2CJrhYzC-nY",
-  authDomain: "careibu-7f119.firebaseapp.com",
-  projectId: "careibu-7f119",
-  storageBucket: "careibu-7f119.firebasestorage.app",
-  messagingSenderId: "668157664675",
-  appId: "1:668157664675:web:e169392bf876a392e80e16",
-  measurementId: "G-BFZSBBK59P"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY as string,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN as string,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID as string,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID as string,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID as string,
 };
 
 // Initialize Firebase
